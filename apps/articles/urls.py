2eec4ba756e92718bbc11c393eee53145cfc29cb
k <k@k.com>
2022-07-26 11:00:28 +0800

    urlpatterns in wrong order

--- apps/articles/urls.py
+++ apps/articles/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import include, url
 from .views import ArticleRecommendationsView, close_article
 
 urlpatterns = [
-    url(r'', ArticleRecommendationsView.as_view(), name='articles'),
     url(r'^close-article/', close_article, name='articles'),
+    url(r'', ArticleRecommendationsView.as_view(), name='articles'),
 ]
